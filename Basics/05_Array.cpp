1ffd117e3797cff6bd3d8f58209cec8871b88d4e
Rohit Rajpurohit <rajpurohit9673@gmail.com>
2025-04-02 20:36:20 +0530

    Added 05 in Basic

---
+++ Basics/05_Array.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[5];
+    arr[0] = 10;
+    cout << sizeof(69)<<endl;
+    // for(int i = 0;i < 5;i++){
+       
+    //     cin >> arr[i];
+    // }
+    // for(int i = 0;i < 5;i++){
+    //     cout << arr[i];
+    // }
+
+    // It gonna give you the size or lenght of Array #IMPORTANT
+    cout << sizeof(arr)/sizeof(arr[0]);
+
+
+    
+}
