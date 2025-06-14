0c2e9642b954692ad3bd37069c7fd8ab85d316c8
rohitrajpurohit17 <rrajpurohit9673@gmail.com>
2025-06-14 19:45:09 +0530

    new patter

--- 02Pattern/levelUpPattern.cpp
+++ 02Pattern/levelUpPattern.cpp
@@ -15,8 +15,70 @@ void triangle(int n){
 
     }
 }
+void triangleRev(int n){
+    
+    for(int i = 0; i < n ; i++){
+        for(int j = 0; j < i; j++){
+            cout << " ";
+        }
+        for(int j=0; j < n*2-(2*i+1); j++ ){
+            cout << "*";
+        }
+        
+        for(int j = 0; j < i; j++){
+            cout << " ";
+        }
+        cout << endl;
+
+    }
+}
+void bothTri(int n ){
+
+     for(int i = 0; i < n; i++){
+        for(int j = n; j > i; j--){
+            cout << " ";
+        }
+        for(int j = 0; j < 2*i-1 ; j++){
+            cout << "*";
+        }
+        for(int j = n; j > i; j--){
+            cout << " ";
+        }
+        cout << endl;
+
+    }
+    for(int i = 0; i < n ; i++){
+        for(int j = 0; j < i; j++){
+            cout << " ";
+        }
+        for(int j=0; j < n*2-(2*i+1); j++ ){
+            cout << "*";
+        }
+        
+        for(int j = 0; j < i; j++){
+            cout << " ";
+        }
+        cout << endl;
+
+    }
+}
+void rightHalfTri(int n){
+    for(int i = 0; i <=2*n-1; i++){
+        int star=i;
+        if( i > n){
+            star = 2*n-i;
+        }
+    for(int j = 0; j < star;j++){
+        cout << "*";
+    }
+    cout << endl;
+    }
+}
 int main(){
 int n;
 cin >> n;
-triangle( n);
+// triangle( n);
+// triangleRev(n);
+// bothTri( n );
+rightHalfTri(n);
 }
